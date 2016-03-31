bc14b6016cae10f385b985650ab0f5581a3ef0da
Alexey Vylegzhanin <alexey.vylegzhanin@yandex.ru>
2016-03-31 17:44:39 +0300

    Finished task with Fibonacci numbers (by Binet formula). Also completed task with getting of Random values

--- Unit-tests/Numbers/ut-fibonacci-numbers.js
+++ Unit-tests/Numbers/ut-fibonacci-numbers.js
@@ -1,7 +1,55 @@
 // Describe function getFibNum()
 describe("Function getFibNum()", function() {
-  it("The 7th element of Fibonacci row is: 13", function() {
-    assert.equal(getFibNum(7), 13);
+
+  describe("Positive scenarios", function() {
+
+    it("The 7th element of Fibonacci row is: 13", function() {
+      assert.equal(getFibNum(7), 13);
+    });
+
+    it("The 65th element of Fibonacci row is: 17167680177565", function() {
+      assert.equal(getFibNum(65), 17167680177565);
+    });
+
+    it("The 0th element of Fibonacci row is: 0", function() {
+      assert.equal(getFibNum(0), 0);
+    });
+
+    it("The 77th element of Fibonacci row is: 5527939700884757", function() {
+      assert.equal(getFibNum(77), 5527939700884757);
+    });
+
   });
-  // continue here <--
+
+  describe("Negative scenarios", function() {
+
+    // continue here later...
+
+  });
+});
+
+// Describe function getFibNumBinet()
+describe("Function getFibNumBinet()", function() {
+
+  describe("Positive scenarios", function() {
+
+    it("The 7th element of Fibonacci row is: 13", function() {
+      assert.equal(getFibNumBinet(7), 13);
+    });
+
+    it("The 65th element of Fibonacci row is: 17167680177565", function() {
+      assert.equal(getFibNumBinet(65), 17167680177565);
+    });
+
+    it("The 0th element of Fibonacci row is: 0", function() {
+      assert.equal(getFibNumBinet(0), 0);
+    });
+
+    // The test fails in cause of rounding error
+    it("The 77th element of Fibonacci row is: 5527939700884757", function() {
+      assert.equal(getFibNumBinet(77), 5527939700884757);
+    });
+
+  });
+
 });
