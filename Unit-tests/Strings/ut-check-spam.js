c1da75d258031d19442801eef8f16a74152060bf
Alexey Vylegzhanin <a.a.vylegzhanin@gmail.com>
2016-04-02 00:19:19 +0300

    Complete few tasks in String/ directory. Need to code-review & unit tests

---
+++ Unit-tests/Strings/ut-check-spam.js
@@ -0,0 +1,16 @@
+// Describe function checkSpam()
+describe("Function checkSpam()", function() {
+
+  describe("Positive scenaris", function() {
+
+    it();
+
+  });
+
+  describe("Negative scenaris", function() {
+
+    it();
+
+  });
+
+});
--- Unit-tests/Strings/ut-first-char-to-upper-case.js
+++ Unit-tests/Strings/ut-first-char-to-upper-case.js
@@ -1,8 +1,32 @@
 // Describe function ucFirst()
-describe("hello", function() {
-  // later...
-  it("world", function() {
-    assert.equal(ucFirst("hello"), "Hello");
+describe("Function ucFirst()", function() {
+
+  describe("Positive scenarios", function() {
+
+    it("If we type type \"hello\" we have: \"Hello\"", function() {
+      assert.equal(ucFirst("hello"), "Hello");
+    });
+
+    it("If we type type \"ucFirst\" we have: \"Ucfirst\"", function() {
+      assert.equal(ucFirst("ucFirst"), "Ucfirst");
+    });
+
   });
-  
+
+  describe("Negative scenarios", function() {
+
+    it("If we type type empty string \"\" we have: \"\"", function() {
+      assert.equal(ucFirst(""), "");
+    });
+
+    it("If we type 0 we have: \"\"", function() {
+      assert.equal(ucFirst(0), "");
+    });
+
+    it("If we any number like 12131 we have: \"\"", function() {
+      assert.equal(ucFirst(12131), "");
+    });
+
+  });
+
 });
