6a88e0dc8111666fbc801c785de591e9a9bc4c12
Alexey Vylegzhanin <a.a.vylegzhanin@gmail.com>
2016-04-03 23:37:13 +0300

    Added not all complete tasks in Objects/. Nedd rewiev and unit tests

---
+++ Unit-tests/Objects. For in/ut-is-empty-object.js
@@ -0,0 +1,12 @@
+// Describe function isEmpty()
+describe("Function isEmpty", function() {
+
+  it("returns true for an empty object", function() {
+    assert.isTrue(isEmpty({}));
+  });
+
+  it("returns false if a property exists", function() {
+    assert.isFalse(isEmpty( {anything: false} ));
+  });
+
+});
