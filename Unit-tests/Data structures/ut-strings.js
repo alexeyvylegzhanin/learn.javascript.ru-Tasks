bad40519d366c51c2fdbcc6e928354c6b7640fce
Alexey Vylegzhanin <alexey.vylegzhanin@yandex.ru>
2016-04-04 17:49:54 +0300

    Completed replacing of all files from Numbers/, Strings/ and Objects/ directories to one separate folder Data strctures/

--- Unit-tests/Data structures/ut-strings.js
+++ Unit-tests/Data structures/ut-strings.js
@@ -36,15 +36,13 @@ describe("Function truncate()", function() {
 
   describe("Positive scenarios", function() {
 
-    it("String: \"Вот, что мне хотелось бы сказать на эту тему:\"
-      with parameter: 20, cuts to: \"Вот, что мне хоте...\"");
+    it("String: \"Вот, что мне хотелось бы сказать на эту тему:\" with parameter: 20, cuts to: \"Вот, что мне хоте...\"", function() {
       assert.equal(truncate("Вот, что мне хотелось бы сказать на эту тему:", 20), "Вот, что мне хоте...");
-    );
+    });
 
-    it("String: \"Всем привет!\"
-      with parameter: 20, cuts to: \"Всем привет!\"");
-      assert.equal(truncate(truncate("Всем привет!", 20), "Всем привет!");
-    );
+    it("String: \"Всем привет!\" with parameter: 20, cuts to: \"Всем привет!\"", function() {
+      assert.equal(truncate("Всем привет!", 20), "Всем привет!");
+    });
 
   });
 
